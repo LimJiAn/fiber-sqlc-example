238cb00b63247c275f9d2e842c7cca78c99fb28d
JiAn-Lim <limjian1990@gmail.com>
2023-09-27 18:17:45 +0900

    Feat: Add posts route

--- api/route/route.go
+++ api/route/route.go
@@ -7,13 +7,14 @@ import (
 
 func SetupRoutes(app *fiber.App) {
 	api := app.Group("/api/v1")
-	// api.Get("/posts", controller.GetPosts)
-	// 	api.Get("/posts/:id", controller.GetPost)
-	// 	api.Post("/posts", controller.NewPost)
-	// 	api.Delete("/posts/:id", controller.DeletePost)
-	// 	api.Put("/posts/:id", controller.UpdatePost)
+	// Post
+	api.Get("/posts", controller.GetPosts)
+	api.Get("/posts/:id", controller.GetPost)
+	api.Post("/posts", controller.NewPost)
+	api.Delete("/posts/:id", controller.DeletePost)
+	api.Put("/posts/:id", controller.UpdatePost)
 
-	// // Author
+	// Author
 	api.Get("/authors", controller.GetAuthors)
 	api.Get("/authors/:id", controller.GetAuthor)
 	api.Post("/authors", controller.NewAuthor)
